f37055ef9d55c9bb39b528c2676e05b732577e3e
Arian Hojat <armyofda12mnkeys@gmail.com>
2020-01-06 05:29:24 -0500

    section4

---
+++ section4/src/App-class.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons: [ 
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Steph', age: 30},
+    ],
+    otherStateProp: 'test'
+  }
+
+  switchNameHandler = (newName) => {
+    console.log('was clicked');
+    /*let newpersons = [...this.state.persons];
+    newpersons[0] = { name: 'MAXXX', age: 66} ;
+    this.setState( {...this.state, persons: newpersons} );*/
+    this.setState(
+      {
+        persons: [ 
+          {name: 'Maxxx'+newName, age: 38},
+          {name: 'Manuuu'+newName, age: 39},
+          {name: 'Stephhh'+newName, age: 40},
+        ]
+      }
+    );
+    
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState(
+      {
+        persons: [ 
+          {name: 'Maxxxy', age: 50},
+          {name: event.target.value, age: 51},
+          {name: 'Stephhhy', age: 52},
+        ]
+      }
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <br/>
+        <button onClick={this.switchNameHandler.bind(this, '-POSTPEND1')}>Switch Name</button>
+        <br/>
+        <button onClick={ () => this.switchNameHandler('-POSTPEND2') }>Switch Name</button>
+        <br/>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+          click={this.switchNameHandler}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, '-POSTPEND1')}  
+          changed={this.nameChangeHandler}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}
+          click={() => this.switchNameHandler('-POSTPEND2')} />
+      </div>
+    );
+    //same as
+    /*
+    return React.createElement('div', {className: 'App'}, 
+      React.createElement('h1', null, 'Hi, I\'m a React App2')
+    );
+    */
+  }
+}
+
+export default App;
---
+++ section4/src/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+
+class App extends Component {
+
+  state = {
+    persons: [ 
+      {id: 101, name: 'Max', age: 28},
+      {id: 102, name: 'Manu', age: 29},
+      {id: 103, name: 'Steph', age: 30},
+    ],
+    otherStateProp: 'test',
+    showPersons : true
+  }
+
+
+  deletePersonHandler = (personIndex) => {
+    console.log('TEST');
+    //const persons = this.state.persons;
+    const persons = [... this.state.persons]; //make a copy vs reference (above is a pointer and mutating state directly which isn't good)
+    persons.splice(personIndex,1);
+    this.setState({
+      persons: persons
+    })
+  }
+  nameChangeHandler = (event, index) => {
+    let personsCopy = [...this.state.persons];
+    let person = personsCopy[index];
+    person.name = event.target.value;
+    personsCopy[index] = person;
+
+    this.setState(
+      {
+        'persons': personsCopy
+      }
+    );
+  }
+  
+  togglePersonsHandler = (event) => {
+    this.setState(
+      {
+        showPersons: !this.state.showPersons
+      }
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+
+        <button onClick={ () => this.togglePersonsHandler() }>Toggle Persons</button>
+        { 
+          this.state.showPersons 
+          ?
+            <div>
+              {this.state.persons.map( (person, index) => {
+                return <Person name={person.name} age={person.age}
+                    key={person.id} id={person.id} 
+                    click={ ()=> this.deletePersonHandler(index) }
+                    changed={ (event) => this.nameChangeHandler(event, index) }
+                  />
+              })}
+             {/*
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+              click={this.switchNameHandler}/>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, '-POSTPEND1')}  
+              changed={this.nameChangeHandler}/>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}
+              click={() => this.switchNameHandler('-POSTPEND2')} />
+            */}
+          </div> 
+          : 
+          null
+        }
+      </div>
+    );
+    //same as
+    /*
+    return React.createElement('div', {className: 'App'}, 
+      React.createElement('h1', null, 'Hi, I\'m a React App2')
+    );
+    */
+  }
+}
+
+export default App;
